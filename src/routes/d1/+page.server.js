6e7328af7ae979a1c960ea63270f6d8e6eb085b2
dag995 <72547889+dag995@users.noreply.github.com>
2025-09-30 18:13:01 +0100

    correction on /d1

--- src/routes/d1/+page.server.js
+++ src/routes/d1/+page.server.js
@@ -1,11 +1,5 @@
-import { call } from '$lib/services/worker'
-
 export const load = async ({ locals, url, parent }) => {
   
-  await parent()
-
-  const c = await call()
-  console.log(c)
- 
+  await parent() 
 
 }
