e6fb8f99bff57d79e31e7243ba6a98e2e37802d5
dag995 <72547889+dag995@users.noreply.github.com>
2025-09-30 16:02:33 +0100

    remove database better sql

--- src/lib/services/database.js
+++ src/lib/services/database.js
@@ -1,30 +1,30 @@
-import { base } from '$app/paths'
-import Database from 'better-sqlite3'
+// import { base } from '$app/paths'
+// import Database from 'better-sqlite3'
 
 
-export class DB {
+// export class DB {
 
-  static database = new Database('database.db')
+//   static database = new Database('database.db')
 
-  static createTable = () => {
+//   static createTable = () => {
 
-    const sql = `CREATE TABLE users (
-      id INTEGER PRIMARY KEY,
-      data,
-      notes,
-      history
-    )`
-    const query = this.database.exec(sql)
-    console.log(`DB Query: ${JSON.stringify(query)} (${sql})`)
+//     const sql = `CREATE TABLE users (
+//       id INTEGER PRIMARY KEY,
+//       data,
+//       notes,
+//       history
+//     )`
+//     const query = this.database.exec(sql)
+//     console.log(`DB Query: ${JSON.stringify(query)} (${sql})`)
 
-  } // create_db()
+//   } // create_db()
 
-  static all = args => this.database.prepare(`SELECT * FROM ${args.type}`).all()
-  static single = args => this.database.prepare(`SELECT * FROM ${args.type} WHERE id = ?`).get(args.id)
-  static insert = args => {
-    const insert = this.database.prepare(`INSERT INTO ${args.type}(data,notes,history) VALUES (?,?,?)`)
-    insert.run([JSON.stringify(args.data), '[]', '[]'])
-  } // insert()
+//   static all = args => this.database.prepare(`SELECT * FROM ${args.type}`).all()
+//   static single = args => this.database.prepare(`SELECT * FROM ${args.type} WHERE id = ?`).get(args.id)
+//   static insert = args => {
+//     const insert = this.database.prepare(`INSERT INTO ${args.type}(data,notes,history) VALUES (?,?,?)`)
+//     insert.run([JSON.stringify(args.data), '[]', '[]'])
+//   } // insert()
 
 
-}
+// }
--- src/routes/data/+page.server.js
+++ src/routes/data/+page.server.js
@@ -1,22 +1,9 @@
-import { DB } from "$lib/services/database"
 
 export const load = async ({ locals, url, parent }) => {
   
   await parent()
 
-  const users = DB.all({ type: 'users' })
-
-  const userMap = users.map(u => {
-    return {
-      id: u.id,
-      data: JSON.parse(u.data),
-      notes: JSON.parse(u.notes),
-      history: JSON.parse(u.history),
-    }
-  })
-
   return {
-    users: userMap,
     fields: {
       name: '',
       role: '',
@@ -30,10 +17,6 @@ export const actions = {
   add: async ({ locals, params, request }) => {
     
     const data = Object.fromEntries(await request.formData())
-    const add = DB.insert({
-      type: 'users',
-      data: data
-    })
 
     return { status: true, message: 'Success' }
 
